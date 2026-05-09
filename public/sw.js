34521f29f282493e05310491900e54a781c831e3
albertodeclaraval <albertodeclaraval@gmail.com>
2026-05-09 19:08:09 -0400

    feat: PWA manifest + service worker

---
+++ public/sw.js
@@ -0,0 +1,29 @@
+var CACHE_NAME = 'little-claraval-v1'
+var urlsToCache = ['/', '/login']
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache)
+    })
+  )
+})
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request)
+    })
+  )
+})
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(name) { return name !== CACHE_NAME })
+          .map(function(name) { return caches.delete(name) })
+      )
+    })
+  )
+})
